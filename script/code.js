19bfd7e70bbc1d3524e41a3271aedbb9b91431db
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-05-19 08:55:31 +0200

    commit

---
+++ script/code.js
@@ -0,0 +1,34 @@
+let numbers = [ 3 ,1 , 5] ;
+let display = numbers;
+console.log(display)
+
+let person1 = {
+    name: 'Name Surname',
+    age: 23,
+    race: 'other'
+}
+let person2 = {
+    gender : 'women'
+   
+}
+
+
+let data = ['lee', 9, 'lithini ',4.7]
+ for(let i = 0 ; i <data.length ; i++) {
+     console.log(data);
+ }
+
+let objArr = [person1]
+let name = ['person','lee','summer'];
+let person = name[0];
+console.log(person);
+
+let sndName = name[1];
+console.log(sndName);
+console.log(objArr);
+
+let objectArr = [person2]
+let gender = ['women','man','other'];
+let genders = gender[0];
+console.log(gender);
+console.log(objArr);
